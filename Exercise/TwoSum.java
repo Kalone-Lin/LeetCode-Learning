0125a94946aba24a7fb87c59501a88daf0602157
shelinbox@outlook.com <lin.she@hand-china.com>
2020-10-29 10:46:21 +0800

    [ADD-1]Two Sum exercise

---
+++ Exercise/TwoSum.java
@@ -0,0 +1,29 @@
+/**
+ * lin.she
+ *
+ * @author shelin 2020/10/29 10:15
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] res = twoSum(nums, target);
+        for (int re : res) {
+            System.out.println(re);
+        }
+    }
+
+    private static int[] twoSum(int[] nums,int target){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+}
