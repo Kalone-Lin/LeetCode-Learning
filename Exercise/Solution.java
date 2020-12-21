30bb1177e277e73b334d8e309ded96434f9bbb15
shelinbox@outlook.com <lin.she@hand-china.com>
2020-12-21 17:14:15 +0800

    [ADD]Merge Two Lists

---
+++ Exercise/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * 有序合并两个序列
+ *
+ * @author shelin 2020/12/21 9:42
+ */
+public class Solution {
+
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode merge = new ListNode();
+
+
+
+        return null;
+    }
+
+
+
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
