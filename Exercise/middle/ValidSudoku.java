7c9f8da03d5a2adc2edd9aded2b2596bf30b5adb
shelinbox@outlook.com <lin.she@hand-china.com>
2021-02-23 14:56:52 +0800

    [ADD]Valid Sudoku

--- Exercise/middle/ValidSudoku.java
+++ Exercise/middle/ValidSudoku.java
@@ -1,6 +1,7 @@
 package middle;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * description
@@ -10,46 +11,52 @@ import java.util.HashSet;
 public class ValidSudoku {
 
     public static void main(String[] args) {
-        String[][] board = {
-                {"8","3",".",".","7",".",".",".","."},
-                {"6",".",".","1","9","5",".",".","."},
-                {".","9","8",".",".",".",".","6","."},
-                {"2",".",".",".","6",".",".",".","3"},
-                {"4",".",".","8",".","3",".",".","1"},
-                {"7",".",".",".","2",".",".",".","6"},
-                {".","6",".",".",".",".","2","8","."},
-                {".",".",".","4","1","9",".",".","5"},
-                {".",".",".",".","8",".",".","7","9"}
+        char[][] board = {
+                {'8','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'2','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'8','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
         };
         boolean validSudoku = isValidSudoku(board);
         System.out.println(validSudoku);
     }
 
 
-    public static boolean isValidSudoku(String[][] board) {
-        boolean flag = true;
+    public static boolean isValidSudoku(char[][] board) {
+        boolean[][] block = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
-
-            HashSet<String> row = new HashSet<>();
-            HashSet<String> column = new HashSet<>();
+            List<Character> row = new ArrayList<>();
+            List<Character> column = new ArrayList<>();
             for (int j = 0; j < board[i].length; j++) {
-                if (!".".equals(board[i][j])) {
+                if ('.' != board[i][j]) {
                     if (row.contains(board[i][j])) {
-                        flag = false;
+                        return false;
                     } else {
                         row.add(board[i][j]);
                     }
+                    int num = board[i][j] - '1';
+                    int blockIndex = i / 3 * 3 + j / 3;
+                    if (block[blockIndex][num]) {
+                        return false;
+                    } else {
+                        block[blockIndex][num] = true;
+                    }
                 }
-                if (!".".equals(board[j][i])) {
+                if ('.' != board[j][i]) {
                     if (column.contains(board[j][i])) {
-                        flag = false;
-                    }else {
+                        return false;
+                    } else {
                         column.add(board[j][i]);
                     }
                 }
             }
         }
-        return flag;
+        return true;
     }
 
 }
